66ea56fd96ff9a7752bdbafeade1a8171819ea5e
Oleksiy Surovtsev <oleksiysurovtsev@gmail.com>
2023-06-28 17:44:30 +0300

    renaming

--- src/test/kotlin/o/sur/app/currencybot/CurrencyBotApplicationTests.kt
+++ src/test/kotlin/o/sur/app/currencybot/CurrencyBotApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MoviesBotApplicationTests {
+class CurrencyBotApplicationTests {
 
     @Test
     fun contextLoads() {
